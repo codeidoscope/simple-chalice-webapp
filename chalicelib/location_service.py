0e445a3d43f472d898fc4ffb5e93165ff92ac371
codeidoscope <codeidoscope@gmail.com>
2023-04-24 14:17:50 +0100

    Add France case

--- chalicelib/location_service.py
+++ chalicelib/location_service.py
@@ -3,5 +3,7 @@ def information_about(place):
         return "London is the capital and largest city of England and the United Kingdom"
     if place == "Oxford":
         return "Oxford is home to the University of Oxford, the oldest university in the English-speaking world"
+    if place == "France":
+        return "France is the place where the burn cars out of aggression towards the government."
 
     return f"Unknown place: {place}"
